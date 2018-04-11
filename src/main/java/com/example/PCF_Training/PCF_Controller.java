bb1f78c2cc8bd7268fe89a614fd4109f8cc72bbb
Ritu Agrawal <ragrawal@solstice.com>
2018-04-11 15:27:12 -0500

    First Commit

---
+++ src/main/java/com/example/PCF_Training/PCF_Controller.java
@@ -0,0 +1,19 @@
+package com.example.PCF_Training;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PCF_Controller {
+
+    @GetMapping("/")
+    public String default_func() {
+        return "default";
+    }
+
+    @GetMapping("/name")
+    public String getName() {
+        return "Ritu";
+    }
+}
---
+++ src/main/java/com/example/PCF_Training/PcfTrainingApplication.java
@@ -0,0 +1,12 @@
+package com.example.PCF_Training;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PcfTrainingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PcfTrainingApplication.class, args);
+	}
+}
